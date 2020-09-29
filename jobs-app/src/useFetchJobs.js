6c0abad8c0bf0870b4a9045ee9962dff347cf0c5
rutvikpatel1234 <tug37127@temple.edu>
2020-09-29 14:43:02 -0400

    Working App

---
+++ jobs-app/src/useFetchJobs.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { useReducer, useEffect } from 'react'
+import axios from 'axios'
+
+const ACTIONS = {
+    MAKE_REQUEST: 'make_request',
+    GET_DATA: 'get_data',
+    ERROR: 'error',
+    UPDATE_HAS_NEXT_PAGE: 'update_has_next_page'
+}
+const BASE_URL = 'https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json'
+
+function reducer(state, action){
+    switch(action.type){
+        case ACTIONS.MAKE_REQUEST:
+            return{ loading: true, jobs: [] }
+        case ACTIONS.GET_DATA:
+            return{ ...state, loading:false, jobs: action.payload.jobs }
+        case ACTIONS.ERROR:
+            return{ ...state, loading:false, error: action.payload.error, jobs: [] }
+        case ACTIONS.UPDATE_HAS_NEXT_PAGE:
+            return{...state, hasNextPage: action.payload.hasNextPage}
+        default:
+            return state
+    }
+}
+
+
+export default function useFetchJobs(params, page){
+    const [state, dispatch]= useReducer(reducer, { jobs: [], loading: true})
+    
+    useEffect(() => {
+        const cancelToken = axios.CancelToken.source()
+        dispatch({ type: ACTIONS.MAKE_REQUEST })
+        axios.get(BASE_URL, {
+            cancelToken: cancelToken.token,
+            params: { markdown: true, page: page, ...params }
+        }).then(res =>{
+            dispatch({ type: ACTIONS.GET_DATA, payload: { jobs: res.data }})
+        }).catch(e => {
+            if (axios.isCancel(e)) return
+            dispatch({ type: ACTIONS.ERROR, payload: { error: e }})
+        })
+        const cancelToken1 = axios.CancelToken.source()
+        axios.get(BASE_URL, {
+            cancelToken: cancelToken1.token,
+            params: { markdown: true, page: page + 1, ...params }
+        }).then(res =>{
+            dispatch({ type: ACTIONS.UPDATE_HAS_NEXT_PAGE, payload: { hasNextPage: res.data.length !== 0 }})
+        }).catch(e => {
+            if (axios.isCancel(e)) return
+            dispatch({ type: ACTIONS.ERROR, payload: { error: e }})
+        })
+        return() => {
+            cancelToken.cancel()
+            cancelToken1.cancel()
+        }
+    }, [params, page])
+
+    return state
+}
